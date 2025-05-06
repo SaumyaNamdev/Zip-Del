c6555a81adf1a913b31816a4f316b4b50bba8106
Saumya <saumyanamdev03@gmail.com>
2025-05-06 21:28:26 +0530

    2.0

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose=require('mongoose');
 const PORT = process.env.PORT || 3000;
-
+const userRoutes=require("./routes/userRoutes");
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+mongoose.connect('mongodb://127.0.0.1:27017/ZipDel')
+.then(()=>console.log("Database Connected"))
+.catch((err)=>console.log("Mongo Error",err));
 
 // Serve all static files from /public
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Default route to home
+app.get("api/auth",userRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "view/index.html"));
 });
